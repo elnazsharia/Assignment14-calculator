fa9e274dda60e042afee87c78d14330af17dc2b0
elnazshariat <elnazshariat1998@gmail.com>
2022-01-05 16:29:16 +0330

    some operations where added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PySide6.QtGui import *
 from PySide6.QtCore import *
 from PySide6.QtUiTools import QUiLoader
 from math import *
+from sympy import *
 
 
 class Calculator (QMainWindow):
@@ -22,11 +23,10 @@ class Calculator (QMainWindow):
         self.ui.btn_pow.clicked.connect(self.Power_num)
         self.ui.btn_jzr.clicked.connect(self.Jazr_num)
         self.ui.btn_delete.clicked.connect(self.Delete_num)
-        self.ui.btn_ce.clicked.connect(self.Clear_num)
-        """self.ui.btn_negPos.clicked.connect(self.Negpos_num)
-        
-        self.ui.btn_sin.clicked.connect(self.Equal_num)
-        self.ui.btn_cos.clicked.connect(self.Equal_num)
+        self.ui.btn_cel.clicked.connect(self.Clear_num)
+        """ self.ui.btn_negPos.clicked.connect(self.Negpos_num) """
+        self.ui.btn_sin.clicked.connect(self.Sin_num)
+        """self.ui.btn_cos.clicked.connect(self.Equal_num)
         self.ui.btn_cot.clicked.connect(self.Equal_num)
         self.ui.btn_tan.clicked.connect(self.Equal_num)
         self.ui.btn_sqrt.clicked.connect(self.Equal_num)
@@ -93,7 +93,45 @@ class Calculator (QMainWindow):
             self.ui.text_box.setText("Not valid")
 
     def Negpos_num(self):
-        pass
+        try:
+            self.first = self.ui.text_box.setText("")
+        except:
+            self.ui.text_box.setText("Not valid")
+
+    def Sin_num(self):
+        try:
+            self.cal = 9
+            self.first = float(self.ui.text_box.text())
+        except:
+            self.ui.text_box.setText("Not valid")
+
+    def Cos_num(self):
+        try:
+            self.cal = 10
+            self.first = float(self.ui.text_box.text())
+        except:
+            self.ui.text_box.setText("Not valid")
+
+    def Log_num(self):
+        try:
+            self.cal = 11
+            self.first = float(self.ui.text_box.text())
+        except:
+            self.ui.text_box.setText("Not valid")
+
+    def Tan_num(self):
+        try:
+            self.cal = 12
+            self.first = float(self.ui.text_box.text())
+        except:
+            self.ui.text_box.setText("Not valid")
+
+    def Cot_num(self):
+        try:
+            self.cal = 13
+            self.first = float(self.ui.text_box.text())
+        except:
+            self.ui.text_box.setText("Not valid")
 
     def Equal_num(self):
         if self.cal == 1:
@@ -127,6 +165,21 @@ class Calculator (QMainWindow):
         if self.cal == 8:
             self.first = self.ui.text_box.setText(
                 str(self.first * 100))
+        if self.cal == 9:
+            self.first = self.ui.text_box.setText(
+                str(sin(self.first)))
+        if self.cal == 10:
+            self.first = self.ui.text_box.setText(
+                str(cos(self.first)))
+        if self.cal == 11:
+            self.first = self.ui.text_box.setText(
+                str(log(self.first)))
+        if self.cal == 12:
+            self.first = self.ui.text_box.setText(
+                str(tan(self.first)))
+        if self.cal == 13:
+            self.first = self.ui.text_box.setText(
+                str(cot(self.first)))
 
 
 if __name__ == "__main__":
