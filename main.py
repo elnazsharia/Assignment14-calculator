9421cee0908d170bd7d526c334b6947f435999f3
elnazshariat <elnazshariat1998@gmail.com>
2022-01-05 13:47:53 +0330

    4 main oprations got compelete

--- main.py
+++ main.py
@@ -8,22 +8,30 @@ class Calculator (QMainWindow):
     def __init__(self):
 
         super().__init__()
-
+        self.res = 0
         loader = QUiLoader()
         self.ui = loader.load("designer.ui")
         self.ui. show()
         self.ui.btn_persent.clicked.connect(self.Persent_Num)
         self.ui.btn_sum.clicked.connect(self.Sum_num)
+        self.ui.btn_equal.clicked.connect(self.Equal_num)
+        self.ui.btn_div.clicked.connect(self.Div_num)
         self.ui.btn_sub.clicked.connect(self.Sub_num)
         self.ui.btn_mul.clicked.connect(self.Mul_num)
-        self.ui.btn_pow.clicked.connect(self.Power_num)
-        self.ui.btn_equal.clicked.connect(self.persent)
+        """self.ui.btn_pow.clicked.connect(self.Power_num)
         self.ui.btn_jazr.clicked.connect(self.Jazr_num)
         self.ui.btn_delete.clicked.connect(self.Delete_num)
         self.ui.btn_ce.clicked.connect(self.Clear_num)
         self.ui.btn_ce.clicked.connect(self.Clear_num)
         self.ui.btn_dot.clicked.connect(self.Dot_num)
         self.ui.btn_negPos.clicked.connect(self.Negpos_num)
+        
+        self.ui.btn_sin.clicked.connect(self.Equal_num)
+        self.ui.btn_cos.clicked.connect(self.Equal_num)
+        self.ui.btn_cot.clicked.connect(self.Equal_num)
+        self.ui.btn_tan.clicked.connect(self.Equal_num)
+        self.ui.btn_sqrt.clicked.connect(self.Equal_num)
+        self.ui.btn_log.clicked.connect(self.Equal_num) """
 
     def Persent_Num(self):
         try:
@@ -33,19 +41,33 @@ class Calculator (QMainWindow):
 
     def Sum_num(self):
         try:
-            pass
+            self.cal = 1
+            self.first = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
         except:
             self.ui.textEdit.setText("Not valid")
 
     def Sub_num(self):
         try:
-            pass
+            self.cal = 2
+            self.first = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
         except:
             self.ui.textEdit.setText("Not valid")
 
     def Mul_num(self):
         try:
-            pass
+            self.cal = 3
+            self.first = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
+        except:
+            self.ui.textEdit.setText("Not valid")
+
+    def Div_num(self):
+        try:
+            self.cal = 4
+            self.first = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
         except:
             self.ui.textEdit.setText("Not valid")
 
@@ -78,6 +100,24 @@ class Calculator (QMainWindow):
 
     def Negpos_num(self):
         pass
+
+    def Equal_num(self):
+        if self.cal == 1:
+            self.second = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
+            self.ui.text_box.setText(str(self.first + self.second))
+        if self.cal == 2:
+            self.second = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
+            self.ui.text_box.setText(str(self.first - self.second))
+        if self.cal == 3:
+            self.second = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
+            self.ui.text_box.setText(str(self.first * self.second))
+        if self.cal == 4:
+            self.second = int(self.ui.text_box.text())
+            self.ui.text_box.setText("")
+            self.ui.text_box.setText(str(self.first / self.second))
 
 
 if __name__ == "__main__":
