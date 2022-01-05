2adec6ace82528ad925175024de6590d64eb27f9
elnazshariat <elnazshariat1998@gmail.com>
2022-01-05 11:59:28 +0330

    methods and btn set in the code

--- main.py
+++ main.py
@@ -12,10 +12,38 @@ class Calculator (QMainWindow):
         loader = QUiLoader()
         self.ui = loader.load("designer.ui")
         self.ui. show()
-        self.ui.btn_persent.clicked.connect(self.persent)
+        self.ui.btn_persent.clicked.connect(self.Persent_Num)
+        self.ui.btn_sum.clicked.connect(self.Sum_num)
+        self.ui.btn_sub.clicked.connect(self.Sub_num)
+        self.ui.btn_mul.clicked.connect(self.persent)
+        self.ui.btn_pow.clicked.connect(self.persent)
+        self.ui.btn_equal.clicked.connect(self.persent)
+        self.ui.btn_jazr.clicked.connect(self.persent)
+        self.ui.btn_delete.clicked.connect(self.persent)
 
-    def persent(self):
+    def Persent_Num(self):
         self.ui.textEdit.setText("")
+
+    def Sum_num(self):
+        pass
+
+    def Sub_num(self):
+        pass
+
+    def Mul_num(self):
+        pass
+
+    def Power_num(self):
+        pass
+
+    def Jazr_num(self):
+        pass
+
+    def Delete_num(self):
+        pass
+
+    def Celere_num(self):
+        pass
 
 
 if __name__ == "__main__":
